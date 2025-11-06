32c3c5bacee4e6ad60fc80cdf2d816ce0175d390
Álvaro <alvaromg8m@gmail.com>
2025-11-06 10:34:44 +0100

    Commit WIP 54 Spiral Matrix

---
+++ top-interview-150/54_Spiral_Matrix.cs
@@ -0,0 +1,24 @@
+// 54. Spiral Matrix
+
+// Given an m x n matrix, return all elements of the matrix in spiral order.
+
+public class Solution {
+    public IList<int> SpiralOrder(int[][] matrix) {
+        List<int> spiral = new List<int>();
+
+        // first row
+        // last column
+        // reverse last row
+        // 
+
+        //signs of operations: +, +, -, -
+        // quantities: L-1, l-1, l-1, l-2,l-2...l-3,l-3
+
+        while(true){
+
+        }
+
+        return spiral;
+
+    }
+}
