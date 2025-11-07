4eab5badbb5b76f06faa1effb323a4a04d8284ef
Álvaro <alvaromg8m@gmail.com>
2025-11-07 18:02:15 +0100

    Commit WIP 2 54 Spiral Matrix

--- top-interview-150/54_Spiral_Matrix.cs
+++ top-interview-150/54_Spiral_Matrix.cs
@@ -5,17 +5,24 @@
 public class Solution {
     public IList<int> SpiralOrder(int[][] matrix) {
         List<int> spiral = new List<int>();
+        int[] operators = {1, 1, -1, -1};
+        int offset = matrix.GetLength(0) - 1;
+        int row = 0;
+        int column = 0;
 
         // first row
         // last column
         // reverse last row
-        // 
 
         //signs of operations: +, +, -, -
         // quantities: L-1, l-1, l-1, l-2,l-2...l-3,l-3
+        // 3 veces enteras, 2 veces con 1 menos...
 
-        while(true){
-
+        while(offset > 0){
+            for(int i = 0; i <= offset; i++){
+                spiral.Add(matrix[row][i]);
+            }
+            offset--;
         }
 
         return spiral;
