35314439341abe2b7c8f6f4f6019a7f4af922777
Álvaro <alvaromg8m@gmail.com>
2025-11-12 09:22:45 +0100

    Commit solution for 122 Best Time to Buy II

--- top-interview-150/Array-String/122_Best_Time_To_Buy_II.cs
+++ top-interview-150/Array-String/122_Best_Time_To_Buy_II.cs
@@ -10,12 +10,11 @@ public class Solution {
         int min = prices[0];
 
         for(int i = 1; i < prices.Length; i++){
-            if(prices[i] < min){
+            if(prices[i] > min){
+                maxProfit += prices[i] - min;
                 min = prices[i];
             }else{
-                if( (prices[i] - min) > maxProfit){
-                    maxProfit = prices[i] - min;
-                }
+                min = prices[i];
             }
         }
 
