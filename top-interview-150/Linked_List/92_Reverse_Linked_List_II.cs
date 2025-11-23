d3e8772929580fe4d2c97a000c7e7ad117d12680
Álvaro <alvaromg8m@gmail.com>
2025-11-23 10:30:27 +0100

    Interviews 💼 Commit optimal solution for 92 Reverse Linked List II

--- top-interview-150/Linked_List/92_Reverse_Linked_List_II.cs
+++ top-interview-150/Linked_List/92_Reverse_Linked_List_II.cs
@@ -2,7 +2,6 @@
 
 // Given the head of a singly linked list and two integers left and right where left <= right, reverse the nodes of the list from position left to position right, and return the reversed list.
 
-
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -16,43 +15,35 @@
  */
 public class Solution {
     public ListNode ReverseBetween(ListNode head, int left, int right) {
-        ListNode newList = new ListNode(-1);
+        ListNode newList = new ListNode(-1, head);
         ListNode pointer = newList;
-        List<ListNode> reverseList = new List<ListNode>();
         int counter = 1;
 
+        if (head == null || head.next == null || left == right)
+            return head;
+        
+
         while(counter < left){
-            pointer.next = head;    // starts assigning 1, counter ends in 1
             pointer = pointer.next;
             
             head = head.next;
             counter++;
         }
+        
+        
+        ListNode beforeLeft = pointer;
+        ListNode first = head;
+        ListNode aux = head.next;
 
-        while(counter <= right){
-            // hacemos pares visitando head
-            reverseList.Add(head);
 
-            head = head.next;
+        while(counter < right && aux != null){
+            first.next = aux.next;
+            aux.next = beforeLeft.next;
+            beforeLeft.next = aux;
+
+
+            aux = first.next;
             counter++;
-        }
-
-        //cambiamos los pares a pointer
-        for (int i = reverseList.Count - 1; i >= 0; i--)
-        {
-            reverseList[i].next = null;
-            pointer.next = reverseList[i];
-            pointer = pointer.next;
-        }
-
-
-
-        // añadimos lo último tras right (si queda)
-        while(head != null){
-            pointer.next = head;
-            pointer = pointer.next;
-            
-            head = head.next;
         }
 
 
