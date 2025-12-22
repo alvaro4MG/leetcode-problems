c07c37941d47db54933c624d2a625018b750933d
Álvaro <alvaromg8m@gmail.com>
2025-12-22 13:20:18 +0100

    Interviews 💼 [C++] Commit 21 Merge Two Sorted Lists

---
+++ top-interview-150/Linked_List/21_Merge_Two_Sorted_Lists.cpp
@@ -0,0 +1,47 @@
+// 21. Merge Two Sorted Lists
+
+// You are given the heads of two sorted linked lists list1 and list2.
+// Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+// Return the head of the merged linked list.
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode dummy = ListNode(-1);
+        ListNode *pointer = &dummy;
+
+        while(list1 != nullptr || list2 != nullptr){
+            
+            if(list1 != nullptr && list2 != nullptr){
+                if(list1->val <= list2->val){
+                    pointer->next = list1;
+                    list1 = list1->next;
+                }else{
+                    pointer->next = list2;
+                    list2 = list2->next;
+                }
+            }else if(list1 != nullptr){
+                pointer->next = list1;
+                list1 = list1->next;
+            }else if(list2 != nullptr){
+                pointer->next = list2;
+                list2 = list2->next;
+            }
+
+            pointer = pointer->next;
+        }
+
+
+        return dummy.next;
+    }
+};
